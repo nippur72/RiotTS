5fcb27e9ab061807dfde190fe7e439d3a87f1ce3
Antonino Porcino <nino.porcino@gmail.com>
2015-07-30 13:31:36 +0200

    Fixed example

--- Test/app.ts
+++ Test/app.ts
@@ -6,8 +6,7 @@ function main() {
       console.log("hash changed to: "+id+"/"+username);
    });
    riot.route.start();
-
-   //riot.class(Timer);
+  
    riot.mount('*');
 
    riot.route("welcome/nino.porcino");
