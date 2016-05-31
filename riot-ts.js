e83f0b45f8ebe84c3af75c26f1e1616b9075a602
Antonino Porcino <nino.porcino@gmail.com>
2016-05-31 16:57:10 +0200

    Renamed main file

--- riot-ts.js
+++ riot-ts.js
@@ -1,120 +1,122 @@
-"use strict";
-var riot = require("riot");
-var compile = require("riot-compiler");
-var Observable = (function () {
-    function Observable() {
-        riot.observable(this);
+System.register(["riot", "riot-compiler"], function(exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    var riot, compile;
+    var Observable, Element, precompiledTags, base;
+    function extend(d, element) {
+        var map = Object.keys(element.prototype).reduce(function (descriptors, key) {
+            descriptors[key] = Object.getOwnPropertyDescriptor(element.prototype, key);
+            return descriptors;
+        }, {});
+        Object.defineProperties(d, map);
     }
-    Observable.prototype.on = function (events, callback) { };
-    Observable.prototype.one = function (events, callback) { };
-    Observable.prototype.off = function (events) { };
-    Observable.prototype.trigger = function (eventName) {
-        var args = [];
-        for (var _i = 1; _i < arguments.length; _i++) {
-            args[_i - 1] = arguments[_i];
+    function registerClass(element) {
+        function registerTag(compiledTag) {
+            var transformFunction = function (opts) {
+                extend(this, element);
+                element.apply(this, [opts]);
+                if (element.prototype.mounted !== undefined)
+                    this.on("mount", this.mounted);
+                if (element.prototype.unmounted !== undefined)
+                    this.on("unmount", this.unmounted);
+                if (element.prototype.updating !== undefined)
+                    this.on("update", this.updating);
+                if (element.prototype.updated !== undefined)
+                    this.on("updated", this.updated);
+            };
+            riot.tag2(compiledTag.tagName, compiledTag.html, compiledTag.css, compiledTag.attribs, transformFunction, riot.settings.brackets);
+            return compiledTag.tagName;
         }
-    };
-    return Observable;
-}());
-exports.Observable = Observable;
-var Element = (function () {
-    function Element() {
-    }
-    Element.prototype.update = function (data) { };
-    Element.prototype.unmount = function (keepTheParent) { };
-    Element.prototype.on = function (eventName, fun) { };
-    Element.prototype.one = function (eventName, fun) { };
-    Element.prototype.off = function (events) { };
-    Element.prototype.trigger = function (eventName) {
-        var args = [];
-        for (var _i = 1; _i < arguments.length; _i++) {
-            args[_i - 1] = arguments[_i];
+        function loadTemplateFromHTTP(template) {
+            var req = new XMLHttpRequest();
+            req.open("GET", template, false);
+            req.send();
+            if (req.status == 200)
+                return req.responseText;
+            else
+                throw req.responseText;
         }
-    };
-    Element.prototype.mixin = function (mixinObject, instance) { };
-    Element.createElement = function (options) {
-        var tagName = this.prototype.tagName;
-        var el = document.createElement(tagName);
-        riot.mount(el, tagName, options);
-        return el;
-    };
-    return Element;
-}());
-exports.Element = Element;
-// new extend, works with getters and setters
-function extend(d, element) {
-    var map = Object.keys(element.prototype).reduce(function (descriptors, key) {
-        descriptors[key] = Object.getOwnPropertyDescriptor(element.prototype, key);
-        return descriptors;
-    }, {});
-    Object.defineProperties(d, map);
-}
-/* old extend, without getters and setters
-function extend(d, element) {
-   Object.keys(element.prototype).forEach((key) => d[key] = element.prototype[key]);
-}
-*/
-exports.precompiledTags = {};
-function registerClass(element) {
-    function registerTag(compiledTag) {
-        var transformFunction = function (opts) {
-            extend(this, element); // copies prototype into "this"                        
-            element.apply(this, [opts]); // calls class constructor applying it on "this"
-            if (element.prototype.mounted !== undefined)
-                this.on("mount", this.mounted);
-            if (element.prototype.unmounted !== undefined)
-                this.on("unmount", this.unmounted);
-            if (element.prototype.updating !== undefined)
-                this.on("update", this.updating);
-            if (element.prototype.updated !== undefined)
-                this.on("updated", this.updated);
-            // TODO support for init(opts) ?
-        };
-        riot.tag2(compiledTag.tagName, compiledTag.html, compiledTag.css, compiledTag.attribs, transformFunction, riot.settings.brackets);
-        return compiledTag.tagName;
-    }
-    function loadTemplateFromHTTP(template) {
-        var req = new XMLHttpRequest();
-        req.open("GET", template, false);
-        req.send();
-        if (req.status == 200)
-            return req.responseText;
-        else
-            throw req.responseText;
-    }
-    ;
-    var compiled;
-    // gets string template: inlined, via http request or via precompiled cache
-    if (element.prototype.template !== undefined) {
-        var tagTemplate = element.prototype.template;
-        if (tagTemplate.indexOf("<") < 0) {
-            // tag is a file
-            if (exports.precompiledTags[tagTemplate] !== undefined) {
-                // loads it from precompiled cache                
-                compiled = exports.precompiledTags[tagTemplate];
+        ;
+        var compiled;
+        if (element.prototype.template !== undefined) {
+            var tagTemplate = element.prototype.template;
+            if (tagTemplate.indexOf("<") < 0) {
+                if (precompiledTags[tagTemplate] !== undefined) {
+                    compiled = precompiledTags[tagTemplate];
+                }
+                else {
+                    tagTemplate = loadTemplateFromHTTP(tagTemplate);
+                    compiled = compile(tagTemplate, true, { entities: true })[0];
+                }
             }
             else {
-                // loads from HTTP and compile on the fly
-                tagTemplate = loadTemplateFromHTTP(tagTemplate);
                 compiled = compile(tagTemplate, true, { entities: true })[0];
             }
+            element.prototype.tagName = registerTag(compiled);
         }
-        else {
-            // tag is inlined, compile on the fly
-            compiled = compile(tagTemplate, true, { entities: true })[0];
-        }
-        element.prototype.tagName = registerTag(compiled);
+        else
+            throw "template property not specified";
     }
-    else
-        throw "template property not specified";
-}
-exports.registerClass = registerClass;
-// @template decorator
-function template(template) {
-    return function (target) {
-        target.prototype["template"] = template;
-        registerClass(target);
-    };
-}
-exports.template = template;
-exports.base = riot;
+    exports_1("registerClass", registerClass);
+    function template(template) {
+        return function (target) {
+            target.prototype["template"] = template;
+            registerClass(target);
+        };
+    }
+    exports_1("template", template);
+    return {
+        setters:[
+            function (riot_1) {
+                riot = riot_1;
+            },
+            function (compile_1) {
+                compile = compile_1;
+            }],
+        execute: function() {
+            Observable = (function () {
+                function Observable() {
+                    riot.observable(this);
+                }
+                Observable.prototype.on = function (events, callback) { };
+                Observable.prototype.one = function (events, callback) { };
+                Observable.prototype.off = function (events) { };
+                Observable.prototype.trigger = function (eventName) {
+                    var args = [];
+                    for (var _i = 1; _i < arguments.length; _i++) {
+                        args[_i - 1] = arguments[_i];
+                    }
+                };
+                return Observable;
+            }());
+            exports_1("Observable", Observable);
+            Element = (function () {
+                function Element() {
+                }
+                Element.prototype.update = function (data) { };
+                Element.prototype.unmount = function (keepTheParent) { };
+                Element.prototype.on = function (eventName, fun) { };
+                Element.prototype.one = function (eventName, fun) { };
+                Element.prototype.off = function (events) { };
+                Element.prototype.trigger = function (eventName) {
+                    var args = [];
+                    for (var _i = 1; _i < arguments.length; _i++) {
+                        args[_i - 1] = arguments[_i];
+                    }
+                };
+                Element.prototype.mixin = function (mixinObject, instance) { };
+                Element.createElement = function (options) {
+                    var tagName = this.prototype.tagName;
+                    var el = document.createElement(tagName);
+                    riot.mount(el, tagName, options);
+                    return el;
+                };
+                return Element;
+            }());
+            exports_1("Element", Element);
+            exports_1("precompiledTags", precompiledTags = {});
+            exports_1("base", base = riot);
+        }
+    }
+});
+//# sourceMappingURL=riot-ts.js.map
